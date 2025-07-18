d6065c724e854bd70401174bc2902a0fe0c7c388
Leducphat1234 <Leducphat1234>
2025-07-18 11:36:16 +0700

    add stuff

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById("menu");
+const imgplayer = document.getElementsByClassName("imgplayer");
+let playerTurn = "x";
+const showPlayerTurn = document.getElementById("playerturn");
+const board = document.getElementById("board");
+
