b132e9ba7962ccae860b3aabbea5490a3c679ae4
Leducphat1234 <Leducphat1234>
2025-08-24 14:09:00 +0700

    start build server

--- server/app.js
+++ server/app.js
@@ -1,2 +1,11 @@
-const express = require("express");
-
+import express from "express";
+const app = express();
+const port = 8080;
+// Define a route for GET requests to the root URL
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World from Express!<h1>");
+});
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
